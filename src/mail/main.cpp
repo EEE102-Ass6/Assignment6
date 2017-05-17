0310d9be45b4f3b53ce71f85e5862688b51349af
Kyrie-Zhao <zhihe.zhao14@student.xjtlu.edu.cn>
2017-05-18 00:01:34 +0800

    Mail
    The codes of sending .txt file to the sina mailbox.

---
+++ src/mail/main.cpp
@@ -0,0 +1,44 @@
+#include "CSendMail.h"
+#include "iostream"
+using namespace std;
+
+
+int main()
+{
+	CSendMail sMailer;
+	//    freopen("F://mailfile//out.txt","w+",stdout);
+
+	sMailer.setServerName("smtp.sina.com");                                                        //邮箱smtp,如"smtp.126.com"
+	sMailer.setUserName("zhaozhihe2333@sina.com");                                                                //邮箱账号名，如"****@126.com"
+	sMailer.setUserPwd("Daohaolaji@");                                                                //邮箱密码
+	sMailer.setSenderName("zzh");                                                                //发件人名字
+	sMailer.setSenderAddress("zhaozhihe2333@sina.com");                                                //发送邮箱地址，填你账号的地址,上面的邮箱账号名"****@126.com"
+	
+	sMailer.setReceiver("zzh","zhaozhihe2333@sina.com");                                            //添加邮件接收者
+	//sMailer.addReceiver("zzh", "397142305@qq.com");
+
+	sMailer.AddFilePath("D:\\canimma.txt");             //添加附件
+															 //    sMailer.AddFilePath("F:/mailfile/libcurl.exp");                                                //添加附件
+
+															 //发送第一份邮件
+	if (sMailer.Connent())                                                                        //每次发邮件前都需要connect
+	{
+		if (sMailer.SendMail("CMailSender:阿垃垃圾君", "你想快速发家致富吗？你想一夜成名吗？访问 http://www.sb.com"))            //第一个字符串是邮件标题，第二个是邮件内容
+			cout << "邮件发送完成！";
+
+	}
+	Sleep(10000);
+	//发送第二份邮件
+	if (sMailer.Connent())                                                                        //每次发邮件前都需要connect
+	{
+		std::string title, content;
+		title = "测试邮件part_2";
+		content = "柯林是个大八嘎！";
+		if (sMailer.SendMail(title, content))                                                        //第一个字符串是邮件标题，第二个是邮件内容
+			cout << "邮件发送完成！";
+
+	}
+
+
+	return 0;
+}
